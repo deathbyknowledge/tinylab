bba12a2b674bce31669a8f0294396fea2208c56e
Steve James <0x2t1ff@gmail.com>
2025-12-26 21:19:01 +0000

    remove torch ew

--- gpt2/model.py
+++ gpt2/model.py
@@ -1,6 +1,7 @@
+from tinygrad.nn.state import torch_load
 from dataclasses import dataclass
 from tinygrad import Tensor, Variable, nn, Context, dtypes, TinyJit, UOp
-from tinygrad.helpers import DEBUG, JIT, getenv
+from tinygrad.helpers import DEBUG, JIT, getenv, fetch
 from typing import Optional, Union
 import tiktoken
 import math
@@ -16,7 +17,7 @@ class MultiHeadAttention:
         self.dim = dim
         self.head_dim = dim // n_heads
 
-    def __call__(self, x: Tensor, start_pos:Variable, mask:Optional[Tensor]) -> Tensor:
+    def __call__(self, x: Tensor, start_pos:UOp, mask:Optional[Tensor]) -> Tensor:
         # x is (B, T, dim)
         if mask is not None or start_pos.val == 0:
             # no symbolic shape qkv when consuming prompts, (prefill?)
@@ -49,11 +50,14 @@ class MultiHeadAttention:
         # tranpose to get seqlen dim at -2 and head_dim at -1
         xq, keys, values = xq.transpose(1, 2), keys.transpose(1,2), values.transpose(1,2)
         # perform attn and tranpose back and reshape -1 to `self.dim`, effectively concatenating back
-        scale = 1.0 / math.sqrt(xq.size(-1))
+        scale = 1.0 / math.sqrt(xq.size(-1))  # ty:ignore[invalid-argument-type]
         attn = xq @ keys.transpose(-2, -1) * scale
 
         if mask is None:
            mask = attn.ones_like(requires_grad=False, device=attn.device, dtype=dtypes.bool).where(0, -float("inf"))
+           # where seems to not inherit dtypes.half, so we need to explicitly cast here
+           if HALF:
+             mask = mask.cast(dtypes.half)
 
         attn += mask
 
@@ -78,7 +82,7 @@ class Block():
     self.ln_2 = nn.LayerNorm(dim)
     self.mlp = MLP(dim, 4*dim) # TODO: read art on why 4 is a good number here
 
-  def __call__(self, x: Tensor, start_pos:Variable, mask:Optional[Tensor]) -> Tensor:
+  def __call__(self, x: Tensor, start_pos:UOp, mask:Optional[Tensor]) -> Tensor:
     x = x + self.attn(self.ln_1(x), start_pos, mask)
     x = x + self.mlp(self.ln_2(x)).contiguous()
     return x
@@ -95,28 +99,26 @@ class GPTConfig:
 class GPT():
   def __init__(self, config: GPTConfig):
     self.config = config
-    self.transformer = dict(
-      wte=nn.Embedding(config.vocab_size, config.n_embd),
-      wpe=nn.Embedding(config.block_size, config.n_embd),
-      h=[Block(config.n_embd, config.n_head) for _ in range(config.n_layer)],
-      ln_f=nn.LayerNorm(config.n_embd),
-    )
+    self.wte = nn.Embedding(config.vocab_size, config.n_embd)
+    self.wpe = nn.Embedding(config.block_size, config.n_embd)
+    self.h = [Block(config.n_embd, config.n_head) for _ in range(config.n_layer)]
+    self.ln_f = nn.LayerNorm(config.n_embd)
     self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
     self.forward_jit = TinyJit(self.forward)
 
 
-  def forward(self, tokens:Union[Tensor,UOp], start_pos:Variable):
+  def forward(self, tokens:Union[Tensor,UOp], start_pos:UOp):
     if not hasattr(self, 'allpos'): self.allpos = Tensor.arange(0, MAX_CONTEXT).reshape(1, -1).realize()
     if isinstance(tokens, UOp):
       seqlen = 1
-      tok_emb = self.transformer['wte'].weight.shrink(((tokens, tokens+1), None))
+      tok_emb = self.wte.weight.shrink(((tokens, tokens+1), None))
     else:
       seqlen = tokens.shape[1]
-      tok_emb = self.transformer['wte'](tokens)
+      tok_emb = self.wte(tokens)
 
     # not symbolic when consuming the prompt
     selected_pos = (0, seqlen) if start_pos.val == 0 else (start_pos, start_pos+1)
-    pos_emb = self.transformer['wpe'](self.allpos.shrink((None, selected_pos)))
+    pos_emb = self.wpe(self.allpos.shrink((None, selected_pos)))
 
     h = tok_emb + pos_emb
 
@@ -124,13 +126,13 @@ class GPT():
 
     mask = Tensor.full((1, 1, seqlen, start_pos.val+seqlen), float("-inf"), dtype=h.dtype).triu(start_pos.val+1) if seqlen > 1 else None
 
-    for hi in self.transformer['h']: h = hi(h, start_pos, mask)
+    for hi in self.h: h = hi(h, start_pos, mask)
 
-    logits = self.lm_head(self.transformer['ln_f'](h)).realize()
+    logits = self.lm_head(self.ln_f(h)).realize()
     return logits
 
 
-  def __call__(self, tokens:Union[Tensor,UOp], start_pos:Variable) -> Tensor:
+  def __call__(self, tokens:Union[Tensor,UOp], start_pos:UOp) -> Tensor:
     forward = (self.forward_jit if JIT and (isinstance(tokens, UOp) or tokens.shape[1] == 1 or tokens.shape[1] == self.config.block_size) else self.forward)
     return forward(tokens, start_pos)
 
@@ -151,36 +153,32 @@ class GPT():
 
     config_args['vocab_size'] = 50257 # always 50257 for GPT model checkpoints
     config_args['block_size'] = 1024 # always 1024 for GPT model checkpoints
+
     # create a from-scratch initialized nanoGPT model
     config = GPTConfig(**config_args)
     model = GPT(config)
     sd = nn.state.get_state_dict(model)
-    sd_keys = sd.keys()
-    sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard the mask buffer
 
     # init a hf transformer
-    from transformers import GPT2LMHeadModel
-    model_hf = GPT2LMHeadModel.from_pretrained(model_type)
-    sd_hf = model_hf.state_dict()
-
-    sd_keys_hf = sd_hf.keys()
-    sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore the mask buffer
-    sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # same
+    weights = torch_load(fetch(f'https://huggingface.co/{model_type}/resolve/main/pytorch_model.bin'))
     transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
 
-    assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
-    for k in sd_keys_hf:
+    for k in weights:
       if any(k.endswith(w) for w in transposed):
         # special treatment for the conv1d weights we need to transpose
-        assert sd_hf[k].shape[::-1] == sd[k].shape
-        sd_hf[k] = sd_hf[k].t()
+        assert weights[k].shape[::-1] == sd[k].shape
+        weights[k] = weights[k].T
 
-      sd[k] = Tensor(sd_hf[k].numpy())
+    weights['lm_head.weight'] = weights['wte.weight']
 
-    nn.state.load_state_dict(model, sd)
+    nn.state.load_state_dict(model, weights)
+    if HALF:
+      for l in nn.state.get_state_dict(model).values():
+        l.replace(l.half().realize())
     return model
 
   def train_step(self, x: Tensor, y: Tensor):
+    start_pos = Variable("start_pos", 0, MAX_CONTEXT-1).bind(0)
     logits = self(x, Variable("start_pos", 0, MAX_CONTEXT-1).bind(0))
     loss = logits.reshape(-1, self.config.vocab_size).cross_entropy(y.reshape(-1)).realize()
     return logits, loss
@@ -218,13 +216,13 @@ class GPT():
 
 
 if __name__ == "__main__":
-  # num_return_sequences = 5
-  # max_length = 30
+  num_return_sequences = 5
+  max_length = 30
 
-  # Tensor.manual_seed(42)
-  # model = GPT.from_pretrained('gpt2')
-  # with Context(BEAM=4):
-  #   model.generate("Hello, I'm a language model,", 30, 5)
+  Tensor.manual_seed(42)
+  model = GPT.from_pretrained('gpt2')
+  with Context(BEAM=2):
+    model.generate("Hello, I'm a language model,", 30, 5)
 
   enc = tiktoken.get_encoding('gpt2')
   with open('input.txt', 'r') as f:
